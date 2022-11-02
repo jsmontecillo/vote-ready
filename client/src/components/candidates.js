13e458a4791f80ab649caa820116aa0ab9d7cb89
jsmontecillo <jsmontecillo@gmail.com>
2022-11-02 13:16:45 -0700

    aligned candidate cards

--- client/src/components/candidates.js
+++ client/src/components/candidates.js
@@ -3,13 +3,13 @@ import CandidateCard from './candidate-card';
 const Candidates = (props) => {
     let candidates = props.candidates;
     return (
-        <>
+        <div style={{width: "1200px", display: "flex"}}>
             {candidates.map((c) => {
             return (
                 <CandidateCard candidate={c} />
             )
             })}
-        </>
+        </div>
     )
 }
 
