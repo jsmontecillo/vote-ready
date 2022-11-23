d3c7bbdb0b9e4ec0d96a66c9cc7dbfbc68bcaf7d
jsmontecillo <jsmontecillo@gmail.com>
2022-11-22 21:17:30 -0800

    fixed mission statement

--- client/src/mission.js
+++ client/src/mission.js
@@ -1,20 +1,22 @@
 import Header from './components/header';
+import vote from './vote.png';
+import Fade from 'react-reveal/Fade';
 
 const Mission = (props) => {
 
     return (
         <>
-            <Header title="Our Mission" />
             <div className="description">
-                I'm baby shabby chic occupy franzen kogi pok pok, tattooed shoreditch readymade iPhone selvage hot chicken. Ascot celiac tumeric kinfolk. Bitters gastropub fit jianbing wayfarers meggings. Bitters cloud bread +1 heirloom whatever meggings jianbing messenger bag green juice hella next level 8-bit keytar.<br/><br/>
-
-                Pinterest put a bird on it lyft tacos, shoreditch +1 affogato mustache irony sustainable banh mi authentic. Hoodie selfies air plant, DIY prism man bun cred asymmetrical +1 vape ascot lumbersexual sus raclette kombucha. Forage helvetica bespoke gatekeep chartreuse whatever master cleanse retro vinyl cronut. Meh normcore marfa actually fam narwhal. Subway tile deep v bitters, poke chicharrones kickstarter literally food truck chambray trust fund pork belly authentic selvage. Lomo slow-carb master cleanse farm-to-table trust fund ennui, put a bird on it try-hard street art tacos you probably haven't heard of them. Pour-over tousled vinyl edison bulb put a bird on it.<br/><br/>
-
-                Marfa selfies woke, mumblecore sustainable organic quinoa pug austin hammock. Vice small batch forage yes plz intelligentsia. Bushwick chambray banh mi paleo retro +1 franzen celiac affogato vibecession single-origin coffee bicycle rights forage. Tattooed plaid mumblecore yes plz hell of shabby chic, raclette mixtape. Synth umami retro post-ironic gentrify meh readymade, fam polaroid cardigan mukbang raw denim. Freegan normcore pug pickled chia deep v DSA health goth tilde leggings. Pug migas freegan tattooed succulents mustache ethical taxidermy.<br/><br/>
-
-                Truffaut affogato letterpress fingerstache lo-fi tacos Brooklyn. Subway tile tacos chambray pork belly succulents hot chicken migas chartreuse. XOXO pitchfork kogi 8-bit. Gluten-free ascot tonx chia. Fixie DIY glossier, hell of actually food truck small batch. Messenger bag green juice 8-bit, shoreditch meditation occupy actually cold-pressed viral schlitz vice lyft celiac sus.<br/><br/>
-
-                Heirloom chia authentic succulents vibecession craft beer single-origin coffee. Chartreuse blue bottle beard cronut locavore biodiesel crucifix kombucha 3 wolf moon asymmetrical. Jianbing kickstarter enamel pin, authentic normcore hell of meditation williamsburg locavore iPhone ramps umami fit fanny pack. Locavore subway tile heirloom next level twee drinking vinegar occupy pinterest craft beer salvia shoreditch ramps pop-up selvage scenester. Vape shabby chic affogato PBR&B. Church-key yes plz air plant, crucifix tattooed disrupt hella sus DSA 3 wolf moon shoreditch. Listicle cornhole church-key scenester bitters vice raclette locavore bruh twee chartreuse artisan.<br/><br/>
+                <Fade right delay={1300}>
+                    <div>
+                        <p style={{position: "absolute", width: "750px"}}>{props.t('mission_statement')}</p>
+                    </div>
+                </Fade>
+                <Fade left delay={1300}>
+                    <div>
+                        <img src={vote} alt="person giving speech" height="800px" style={{position: "relative", marginLeft: "200px", zIndex: "-10"}} />
+                    </div>
+              </Fade>
             </div>
         </>
     )
